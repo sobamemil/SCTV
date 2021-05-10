b1b943a165c0e94dfed02d1c77f35a08b78d342b
sobamemil <sieh96@naver.com>
2021-05-10 15:34:28 +0900

    CBCentralManagerState의 사용이 금지되고 CBManagerState를 사용하는 것으로 변경되어 해당 코드 수정 CBCentralManagerState.PoweredOn을 CBManagerState.poweredOn으로 변경하여 사용 (알파벳 p의 대소문자 구분 주의)

--- SCTV/ViewController.swift
+++ SCTV/ViewController.swift
@@ -8,57 +8,114 @@
 import UIKit
 import CoreBluetooth // 블루투스 사용
 
+//class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    @IBOutlet weak var sendTextField: UITextField!
+//
+//    @IBAction func sendButton(_ sender: Any) {
+//        if let text = sendTextField{
+//                writeValue(sendTextField.text)
+//            }
+//    }
+//
+//    // 상태가 변하면(ex, Bluetooth is turned off) 자동으로 호출되는 콜백 메소드
+//    func centralManagerDidUpdateState(central: CBCentralManager?) {
+//        if let central = central{
+//            if central.state == CBCentralManagerState.PoweredOn {
+//                // 다양한 로직 구현 가능
+//                // 여기에선 최대한 simple하게 작성
+//                println("Bluetooth ON")
+//            }
+//            else {
+//                println("Bluetooth switched off or not initialized")
+//            }
+//        }
+//    }
+//
+//    // 어떤 데이터든 들어오면 잡아주는 메소드
+//    // 여기에선 navigation bar item에 출력해줌
+//    // Get data values when they are updated
+//    func peripheral(peripheral: CBPeripheral?, didUpdateValueForCharacteristic characteristic: CBCharacteristic?, error: NSError!) {
+//        if let characteristicValue = characteristic?.value{
+//            var datastring = NSString(data: characteristicValue, encoding: NSUTF8StringEncoding)
+//            if let datastring = datastring{
+//                navigationItem.title = datastring as String
+//            }
+//        }
+//    }
+//
+//    // 데이터를 작성하여 블루투스 디바이스로 전송해주는 메소드
+//    // Write function
+//    func writeValue(data: String){
+//        let data = (data as NSString).dataUsingEncoding(NSUTF8StringEncoding)
+//        if let peripheralDevice = peripheralDevice{
+//            if let deviceCharacteristics = deviceCharacteristics{
+//                peripheralDevice.writeValue(data, forCharacteristic:
+//        deviceCharacteristics, type: CBCharacteristicWriteType.WithoutResponse)
+//            }
+//        }
+//    }
+//}
+
+import UIKit
+import CoreBluetooth
+
 class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+@IBAction func sendButton(sender: AnyObject) {
+if let text = sendTextField{
+writeValue(sendTextField.text)
+        }
     }
-
-    @IBOutlet weak var sendTextField: UITextField!
+@IBOutlet weak var sendTextField: UITextField!
     
-    @IBAction func sendButton(_ sender: Any) {
-        if let text = sendTextField{
-                writeValue(sendTextField.text)
-            }
+override func viewDidLoad() {
+super.viewDidLoad()
+// Do any additional setup after loading the view, typically from a nib.
     }
-    
-    // 상태가 변하면(ex, Bluetooth is turned off) 자동으로 호출되는 콜백 메소드
-    func centralManagerDidUpdateState(central: CBCentralManager?) {
-        if let central = central{
-            if central.state == CBCentralManagerState.PoweredOn {
-                // 다양한 로직 구현 가능
-                // 여기에선 최대한 simple하게 작성
-                println("Bluetooth ON")
+    override func viewWillAppear(_ animated: Bool) {
+if let activeCentralManager = activeCentralManager{
+            activeCentralManager.delegate = self
+        }
+if let peripheralDevice = peripheralDevice{
+            peripheralDevice.delegate = self
+        }
+    }
+override func didReceiveMemoryWarning() {
+super.didReceiveMemoryWarning()
+// Dispose of any resources that can be recreated.
+    }
+    func centralManagerDidUpdateState(_ central: CBCentralManager?) {
+if let central = central{
+if central.state == CBManagerState.poweredOn {
+    print("Bluetooth ON")
             }
-            else {
-                println("Bluetooth switched off or not initialized")
+else {
+// Can have different conditions for all states if needed - print generic message for now
+                print("Bluetooth switched off or not initialized")
             }
         }
     }
-    
-    // 어떤 데이터든 들어오면 잡아주는 메소드
-    // 여기에선 navigation bar item에 출력해줌
-    // Get data values when they are updated
+// Get data values when they are updated
     func peripheral(peripheral: CBPeripheral?, didUpdateValueForCharacteristic characteristic: CBCharacteristic?, error: NSError!) {
-        if let characteristicValue = characteristic?.value{
-            var datastring = NSString(data: characteristicValue, encoding: NSUTF8StringEncoding)
-            if let datastring = datastring{
+if let characteristicValue = characteristic?.value{
+var datastring = NSString(data: characteristicValue, encoding: NSUTF8StringEncoding)
+if let datastring = datastring{
                 navigationItem.title = datastring as String
             }
         }
     }
-    
-    // 데이터를 작성하여 블루투스 디바이스로 전송해주는 메소드
-    // Write function
-    func writeValue(data: String){
-        let data = (data as NSString).dataUsingEncoding(NSUTF8StringEncoding)
-        if let peripheralDevice = peripheralDevice{
-            if let deviceCharacteristics = deviceCharacteristics{
-                peripheralDevice.writeValue(data, forCharacteristic:
-        deviceCharacteristics, type: CBCharacteristicWriteType.WithoutResponse)
+// Write function
+    func writeValue(_ data: String?){
+let data = (data as! NSString).dataUsingEncoding(NSUTF8StringEncoding)
+if let peripheralDevice = peripheralDevice{
+if let deviceCharacteristics = deviceCharacteristics{
+                peripheralDevice.writeValue(data, forCharacteristic: deviceCharacteristics, type: CBCharacteristicWriteType.WithoutResponse)
             }
         }
     }
 }
-
