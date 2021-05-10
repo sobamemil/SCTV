9fb3f4c646100a8b4ce5c653191c13586474c94c
sobamemil <sieh96@naver.com>
2021-05-10 23:18:54 +0900

    ios 블루투스 기능 구현.. 어려움

--- SCTV/ViewController.swift
+++ SCTV/ViewController.swift
@@ -1,13 +1,13 @@
+////
+////  ViewController.swift
+////  SCTV
+////
+////  Created by 심찬영 on 2021/05/09.
+////
 //
-//  ViewController.swift
-//  SCTV
+//import UIKit
+//import CoreBluetooth // 블루투스 사용
 //
-//  Created by 심찬영 on 2021/05/09.
-//
-
-import UIKit
-import CoreBluetooth // 블루투스 사용
-
 //class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
 //
 //    override func viewDidLoad() {
@@ -19,22 +19,22 @@ import CoreBluetooth // 블루투스 사용
 //
 //    @IBAction func sendButton(_ sender: Any) {
 //        if let text = sendTextField{
-//                writeValue(sendTextField.text)
+//            writeValue(data: sendTextField.text)
 //            }
 //    }
 //
 //    // 상태가 변하면(ex, Bluetooth is turned off) 자동으로 호출되는 콜백 메소드
-//    func centralManagerDidUpdateState(central: CBCentralManager?) {
-//        if let central = central{
-//            if central.state == CBCentralManagerState.PoweredOn {
+//    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+////        if let central = central {
+//            if central.state == CBManagerState.poweredOn {
 //                // 다양한 로직 구현 가능
 //                // 여기에선 최대한 simple하게 작성
-//                println("Bluetooth ON")
+//                print("Bluetooth ON")
 //            }
 //            else {
-//                println("Bluetooth switched off or not initialized")
+//                print("Bluetooth switched off or not initialized")
 //            }
-//        }
+//        //}
 //    }
 //
 //    // 어떤 데이터든 들어오면 잡아주는 메소드
@@ -42,8 +42,8 @@ import CoreBluetooth // 블루투스 사용
 //    // Get data values when they are updated
 //    func peripheral(peripheral: CBPeripheral?, didUpdateValueForCharacteristic characteristic: CBCharacteristic?, error: NSError!) {
 //        if let characteristicValue = characteristic?.value{
-//            var datastring = NSString(data: characteristicValue, encoding: NSUTF8StringEncoding)
-//            if let datastring = datastring{
+//            var datastring = NSString(data: characteristicValue, encoding: CFStringConvertEncodingToNSStringEncoding(0x0422))
+//            if let datastring = datastring {
 //                navigationItem.title = datastring as String
 //            }
 //        }
@@ -51,71 +51,41 @@ import CoreBluetooth // 블루투스 사용
 //
 //    // 데이터를 작성하여 블루투스 디바이스로 전송해주는 메소드
 //    // Write function
-//    func writeValue(data: String){
-//        let data = (data as NSString).dataUsingEncoding(NSUTF8StringEncoding)
-//        if let peripheralDevice = peripheralDevice{
-//            if let deviceCharacteristics = deviceCharacteristics{
-//                peripheralDevice.writeValue(data, forCharacteristic:
-//        deviceCharacteristics, type: CBCharacteristicWriteType.WithoutResponse)
+//    func writeValue(data: String?){
+//        let data = (data! as NSString).data(using: CFStringConvertEncodingToNSStringEncoding(0x0422))
+//        if let peripheralDevice = peripheralDevice {
+//            if let deviceCharacteristics = deviceCharacteristics {
+//                peripheralDevice.writeValue(data!, for: deviceCharacteristics, type: CBCharacteristicWriteType.withoutResponse)
 //            }
 //        }
 //    }
+//
+//    override func viewWillAppear(_ animated: Bool) {
+//        if let activeCentralManager = activeCentralManager{
+//            activeCentralManager.delegate = self
+//        }
+//        if let peripheralDevice = peripheralDevice{
+//            peripheralDevice.delegate = self
+//        }
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 //}
+//
+//
+//
+//
 
 import UIKit
 import CoreBluetooth
 
-class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
-@IBAction func sendButton(sender: AnyObject) {
-if let text = sendTextField{
-writeValue(sendTextField.text)
-        }
-    }
-@IBOutlet weak var sendTextField: UITextField!
-    
-override func viewDidLoad() {
-super.viewDidLoad()
-// Do any additional setup after loading the view, typically from a nib.
-    }
-    override func viewWillAppear(_ animated: Bool) {
-if let activeCentralManager = activeCentralManager{
-            activeCentralManager.delegate = self
-        }
-if let peripheralDevice = peripheralDevice{
-            peripheralDevice.delegate = self
-        }
-    }
-override func didReceiveMemoryWarning() {
-super.didReceiveMemoryWarning()
-// Dispose of any resources that can be recreated.
-    }
-    func centralManagerDidUpdateState(_ central: CBCentralManager?) {
-if let central = central{
-if central.state == CBManagerState.poweredOn {
-    print("Bluetooth ON")
-            }
-else {
-// Can have different conditions for all states if needed - print generic message for now
-                print("Bluetooth switched off or not initialized")
-            }
-        }
-    }
-// Get data values when they are updated
-    func peripheral(peripheral: CBPeripheral?, didUpdateValueForCharacteristic characteristic: CBCharacteristic?, error: NSError!) {
-if let characteristicValue = characteristic?.value{
-var datastring = NSString(data: characteristicValue, encoding: NSUTF8StringEncoding)
-if let datastring = datastring{
-                navigationItem.title = datastring as String
-            }
-        }
-    }
-// Write function
-    func writeValue(_ data: String?){
-let data = (data as! NSString).dataUsingEncoding(NSUTF8StringEncoding)
-if let peripheralDevice = peripheralDevice{
-if let deviceCharacteristics = deviceCharacteristics{
-                peripheralDevice.writeValue(data, forCharacteristic: deviceCharacteristics, type: CBCharacteristicWriteType.WithoutResponse)
-            }
-        }
+class BluetoothViewController : UIViewController {
+    private var centralManager : CBCentralManager!
+    override func viewDidLoad () {
+        슈퍼 . viewDidLoad ()
+        centralManager = CBCentralManager ( delegate : self , queue : nil )
     }
 }
